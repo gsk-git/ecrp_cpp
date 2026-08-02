578de62564b3c1af11c38d28a0fd5ab701cdcf41
gsk-git <gauthamsevar@gmail.com>
2026-08-02 17:54:09 +0530

    Refractor changes update

--- out/build/x64-Debug/_deps/catch2-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Sevarkodiyon/Documents/ecrp_cpp/out/build/x64-Debug/_deps/catch2-src
-# Build directory: C:/Users/Sevarkodiyon/Documents/ecrp_cpp/out/build/x64-Debug/_deps/catch2-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
